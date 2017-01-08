5428543ace1e71326dc285205567261396b882a8
linux_china <linux_china@hotmail.com>
2017-01-08 21:44:53 +0800

    add listener

--- src/main/java/org/mvnsearch/cloud/KafkaDemoApplication.java
+++ src/main/java/org/mvnsearch/cloud/KafkaDemoApplication.java
@@ -2,11 +2,21 @@ package org.mvnsearch.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.cloud.stream.messaging.Source;
 
 @SpringBootApplication
+@EnableBinding({Sink.class, Source.class})
 public class KafkaDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KafkaDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaDemoApplication.class, args);
+    }
+
+    @StreamListener(Sink.INPUT)
+    public void log(String payload) {
+        System.out.println(payload);
+    }
 }
