f9b37254176b625f362f9b4f5a3605cdb3c718f5
linux_china <linux_china@hotmail.com>
2017-02-10 08:46:16 +0800

    Polishing

--- src/test/java/org/mvnsearch/cloud/MessageOutputTest.java
+++ src/test/java/org/mvnsearch/cloud/MessageOutputTest.java
@@ -5,9 +5,9 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.stream.messaging.Source;
-import org.springframework.integration.support.MessageBuilder;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.test.context.junit4.SpringRunner;
 
 /**
