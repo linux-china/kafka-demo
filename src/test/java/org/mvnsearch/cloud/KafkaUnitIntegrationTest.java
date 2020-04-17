9a5262ec5207ae0f4dacbdbd5a10f0930140a988
linux_china <libing.chen@gmail.com>
2020-04-16 18:02:31 -0700

    introduce Kafka JUnit

--- src/test/java/org/mvnsearch/cloud/KafkaUnitIntegrationTest.java
+++ src/test/java/org/mvnsearch/cloud/KafkaUnitIntegrationTest.java
@@ -1,27 +1,31 @@
 package org.mvnsearch.cloud;
 
-import info.batey.kafka.unit.KafkaUnitRule;
+import com.salesforce.kafka.test.KafkaTestUtils;
+import com.salesforce.kafka.test.junit5.SharedKafkaTestResource;
+import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.junit.Rule;
-import org.junit.Test;
-
-import java.util.Arrays;
-import java.util.List;
-
-import static org.junit.Assert.assertEquals;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 
 public class KafkaUnitIntegrationTest {
 
-    @Rule
-    public KafkaUnitRule kafkaUnitRule = new KafkaUnitRule();
+    @RegisterExtension
+    public static final SharedKafkaTestResource sharedKafkaTestResource = new SharedKafkaTestResource()
+            .withBrokerProperty("auto.create.topics.enable", "true")
+            .withBrokerProperty("message.max.bytes", "512000");
 
     @Test
     public void junitRuleShouldHaveStartedKafka() throws Exception {
         String testTopic = "TestTopic";
-        kafkaUnitRule.getKafkaUnit().createTopic(testTopic);
-        ProducerRecord<String, String> keyedMessage = new ProducerRecord<>(testTopic, "key", "value");
-        kafkaUnitRule.getKafkaUnit().sendMessages(keyedMessage);
-        List<String> messages = kafkaUnitRule.getKafkaUnit().readMessages(testTopic, 1);
-        assertEquals(Arrays.asList("value"), messages);
+        KafkaTestUtils kafkaTestUtils = sharedKafkaTestResource.getKafkaTestUtils();
+        kafkaTestUtils.createTopic(testTopic, 1, (short) 1);
+        //send message
+        ProducerRecord<String, String> keyedMessage = new ProducerRecord<>(testTopic, "key", "Jakie");
+        KafkaProducer<String, String> kafkaProducer = kafkaTestUtils.getKafkaProducer(StringSerializer.class, StringSerializer.class);
+        kafkaProducer.send(keyedMessage);
+        kafkaTestUtils.consumeAllRecordsFromTopic(testTopic, StringDeserializer.class, StringDeserializer.class)
+                .forEach(record -> System.out.println(record.value()));
     }
 }
