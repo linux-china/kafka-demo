39a6bed3efb0bd4125d066cd96844345b26b32e9
linux_china <libing.chen@gmail.com>
2021-11-28 13:28:28 -0800

    Adjust to function style

--- src/main/java/org/mvnsearch/cloud/KafkaDemoApplication.java
+++ src/main/java/org/mvnsearch/cloud/KafkaDemoApplication.java
@@ -2,21 +2,19 @@ package org.mvnsearch.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.annotation.StreamListener;
-import org.springframework.cloud.stream.messaging.Sink;
-import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Consumer;
 
 @SpringBootApplication
-@EnableBinding({Sink.class, Source.class})
 public class KafkaDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KafkaDemoApplication.class, args);
     }
 
-    @StreamListener(Sink.INPUT)
-    public void log(String payload) {
-        System.out.println("Payload:" + payload);
+    @Bean
+    public Consumer<String> sink() {
+        return payload -> System.out.println("Payload:" + payload);
     }
 }
