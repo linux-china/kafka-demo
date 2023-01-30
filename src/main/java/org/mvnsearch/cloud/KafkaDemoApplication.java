19c7c2da1f94c640737bdef31e76917ac546fb9b
Libing Chen <libing.chen@gmail.com>
2023-01-30 14:52:28 +0800

    refactor: move event processor to EventConfiguration

--- src/main/java/org/mvnsearch/cloud/KafkaDemoApplication.java
+++ src/main/java/org/mvnsearch/cloud/KafkaDemoApplication.java
@@ -2,9 +2,6 @@ package org.mvnsearch.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.util.function.Consumer;
 
 @SpringBootApplication
 public class KafkaDemoApplication {
@@ -13,8 +10,4 @@ public class KafkaDemoApplication {
         SpringApplication.run(KafkaDemoApplication.class, args);
     }
 
-    @Bean
-    public Consumer<String> sink() {
-        return payload -> System.out.println("Payload:" + payload);
-    }
 }
